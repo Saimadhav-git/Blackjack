61973229a9bc0f49c05414189bfcede8af1ddc24
Saimadhav <Saimadhav-git>
2020-10-10 22:16:26 +0530

    first commit

---
+++ blackjack/import_test.py
@@ -0,0 +1,9 @@
+import blackjack
+
+blackjack._deal_card(blackjack.dealer_card_frame)
+blackjack.play()
+
+print(__name__)
+print(blackjack.cards)
+
+mk
---
+++ blackjack/spam.py
@@ -0,0 +1,7 @@
+def spam():
+    def spam2():
+        def spam3():
+
+        z = 'even'
+        z += y
+        print('In spam3, locals are {}'.format(locals()))
